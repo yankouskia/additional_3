eeb9fb01c68a3ed35bf7ae979252525b1a25bcc3
alnepravskaya <alnepravskaya@gmail.com>
2017-03-11 21:58:56 +0300

    tricky tests

--- test.js
+++ test.js
@@ -56,3 +56,99 @@ it('should calculate minimal base for 2391484', () => {
 it('should calculate minimal base for 1000000000000000000', () => {
   assert.equal(getBase('1000000000000000000'), '999999999999999999');
 });
+let overload = Number.MAX_SAFE_INTEGER;
+
+describe("Manual tests", function(){
+  it ("test base < 10", function(){
+    assert.equal(getBase(3), 2);
+    assert.equal(getBase(7), 2);
+    assert.equal(getBase(21), 4);
+    assert.equal(getBase(57), 7);
+    assert.equal(getBase(1111), 10);
+  });
+
+  it ("big base tests", function(){
+    assert.equal(getBase(1000002), 1000001);
+    assert.equal(getBase(1000000002), 1000000001);
+    assert.equal(getBase(1000000000000), 999999999999);
+  });
+
+  it ("tricky part : )", function(){
+    assert.equal(getBase(1001001), 1000);
+    assert.equal(getBase(1001001001), 1000);
+    assert.equal(getBase(1001001001001), 1000);
+
+    assert.equal(getBase(2500050001), 50000);
+    assert.equal(getBase(125002500050001), 50000);
+  });
+
+
+});
+
+describe("Random tests", function () {
+  it ("random test with base < 10", function(){
+    for (let i=0; i<100; i++) {
+      let n = 2 + ~~(Math.random()*8);
+      let num = 1 + n + n*n + n*n*n + n*n*n*n;
+      assert.equal(getBase(num), n);
+    }
+  });
+
+  it ("random test with base < 1000", function(){
+    for (let i=0; i<100; i++) {
+      let n = 2 + ~~(Math.random()*998);
+      let num = 1 + n + n*n + n*n*n + n*n*n*n;
+      assert.equal(getBase(num), n);
+    }
+  });
+
+  it ("random test with base < 10000", function(){
+    for (let i=0; i<100; i++) {
+      let n = 2 + ~~(Math.random()*9998);
+      let num = 1 + n + n*n + n*n*n;
+      assert.equal(getBase(num), n);
+    }
+  });
+
+  it ("random big base tests", function(){
+    for (let i=0; i<10; i++) {
+      let n = ~~(Math.sqrt(overload) + Math.random()*Math.sqrt(overload));
+      let num = 1 + n;
+      assert.equal(getBase(num), n);
+    }
+  });
+
+  it ("random tricky part : )", function(){
+    for (let i=0; i<5; i++) {
+      let n = 2 + ~~(Math.random()*Math.sqrt(overload));
+      let num = 1 + n + n*n;
+      if (num < overload) {
+        assert.equal(getBase(num), n);
+      }
+    }
+
+    for (let i=0; i<5; i++) {
+      let n = 2 + ~~(Math.random()*Math.pow(overload, 1/3));
+      let num = 1 + n + n*n + n*n*n;
+      if (num < overload) {
+        assert.equal(getBase(num), n);
+      }
+    }
+
+    for (let i=0; i<5; i++) {
+      let n = 2 + ~~(Math.random()*Math.pow(overload, 1/4));
+      let num = 1 + n + n*n + n*n*n + n*n*n*n;
+      if (num < overload) {
+        assert.equal(getBase(num), n);
+      }
+    }
+
+    for (let i=0; i<5; i++) {
+      let n = 2 + ~~(Math.random()*Math.pow(overload, 1/5));
+      let num = 1 + n + n*n + n*n*n + n*n*n*n + n*n*n*n*n;
+      if (num < overload) {
+        assert.equal(getBase(num), n);
+      }
+    }
+  });
+});
